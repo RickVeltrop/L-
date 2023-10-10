8eb7a231924234a736b4bc633160c772944e0b2e
RickVeltrop <75987001+RickVeltrop@users.noreply.github.com>
2023-10-10 08:26:34 +0000

    Updated main file

--- main.lua
+++ main.lua
@@ -3,16 +3,12 @@ using "system.Diagnostics";
 using "system.Threading";
 using "system.IO";
 
-class("Program", {
+class("Program",
+{
 	var = "variable";
-
-	gay = function()
-		Console.WriteLine("u are gayyyyy boyyyyyy");
-	end;
 
 	Main = function(args)
 		local str = f"This is a {var}!";
 		Console.WriteLine(str);
-		Console.WriteLine(f"{gay()}");
 	end;
 })
