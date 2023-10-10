f6afcb3db4a8f1fa40b2ce214245bf6c149a668f
RickVeltrop <75987001+RickVeltrop@users.noreply.github.com>
2023-10-10 08:06:33 +0000

    Initial commit

--- main.lua
+++ main.lua
@@ -6,8 +6,13 @@ using "system.IO";
 class("Program", {
 	var = "variable";
 
+	gay = function()
+		Console.WriteLine("u are gayyyyy boyyyyyy");
+	end;
+
 	Main = function(args)
 		local str = f"This is a {var}!";
 		Console.WriteLine(str);
-	end
+		Console.WriteLine(f"{gay()}");
+	end;
 })
